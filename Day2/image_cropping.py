4941ca6e0474b3ad4556809793ea95b497ccac49
GOPINATH THANGAVEL <gopinaththangavel10@gmail.com>
2026-03-03 10:36:01 +0530

    Day2's Upadate started image cropping

---
+++ Day2/image_cropping.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('wallpaper.jpg')
+cv2.imshow('original_image', img)
+row,col,chan = img.shape
+print('rows : ', row)
+print('columns : ', col)
+
+# Cropped image
+cropped_image = img[100:500, 800:1100]
+cv2.imshow('cropped_image', cropped_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
