a737045864b1d3805cbeb5982e159aaa2fba8a88
GOPINATH THANGAVEL <gopinaththangavel10@gmail.com>
2026-03-05 16:36:50 +0530

    Blob Detection file Added

---
+++ Day4/blob.py
@@ -0,0 +1,37 @@
+import cv2
+import numpy as np
+
+im = cv2.imread('BlobTest.jpg',0)
+
+params = cv2.SimpleBlobDetector_Params()
+
+#Thresholds
+params.minThreshold = 10
+params.maxThreshold = 200
+
+#Filter by Area
+params.filterByArea = True
+params.minArea = 1500
+
+#Filter by Circularity
+params.filterByCircularity = True
+params.minCircularity = 0.1
+
+#Filter by Convexity
+params.filterByConvexity = True
+params.minConvexity = 0.87
+
+#Filter by Inertia
+params.filterByInertia = True
+params.minInertiaRatio = 0.01
+
+ver = (cv2.__version__).split('.')
+if int(ver[0]) < 3 :
+    detector = cv2.SimpleBlobDetector(params)
+else:
+    detector = cv2.SimpleBlobDetector_create(params)
+keypoints = detector.detect(im)
+im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+cv2.imshow("Keypoints", im_with_keypoints)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
