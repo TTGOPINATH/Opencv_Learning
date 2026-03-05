4c1afe81f597a09dcaa4a6ae6e526b20662dce7e
GOPINATH THANGAVEL <gopinaththangavel10@gmail.com>
2026-03-05 19:23:58 +0530

    contour file Reter_Tree added image

--- Day4/contour_detect.py
+++ Day4/contour_detect.py
@@ -7,7 +7,7 @@ img = cv2.imread('contour.jpg', cv2.IMREAD_GRAYSCALE)
 _, thresh = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
 
 # Find contours
-contours1, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+contours1, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 contours2, _ = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
 
 # Draw contours on a copy of the original image
