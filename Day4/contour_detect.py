c8b054cca1613b95aa63aa5cf0110296b6fb9d69
GOPINATH THANGAVEL <gopinaththangavel10@gmail.com>
2026-03-05 19:17:51 +0530

    Contour Detection File Added

---
+++ Day4/contour_detect.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('contour.jpg', cv2.IMREAD_GRAYSCALE)
+
+# Threshold the image to binary
+_, thresh = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+
+# Find contours
+contours1, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+contours2, _ = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+
+# Draw contours on a copy of the original image
+img_contours = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+cv2.drawContours(img_contours, contours1, -1, (0, 255, 0), 2)
+cv2.drawContours(img_contours, contours2, -1, (0, 0, 255), 1)
+# Display the original and contour images
+cv2.imshow('Original Image', img)
+cv2.imshow('Contours Detected', img_contours)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+cv2.imwrite('contours_detected.jpg', img_contours)
