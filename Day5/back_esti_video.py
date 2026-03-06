49a543720df4c80edf251e53150c3eceff8b5fed
GOPINATH THANGAVEL <gopinaththangavel10@gmail.com>
2026-03-06 19:25:46 +0530

    Background Estimation Or Background Subtraction File Added

---
+++ Day5/back_esti_video.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+vue = cv2.VideoCapture('video1.mp4')
+# Background Subtraction
+fgbg = cv2.createBackgroundSubtractorMOG2()
+while True:
+    ret, frame = vue.read()
+    if not ret:
+        break
+    fgmask = fgbg.apply(frame)
+    # Display the original video and the foreground mask
+    cv2.imshow('Original Video', frame)
+    cv2.imshow('Foreground Mask', fgmask)
+    if cv2.waitKey(30) & 0xFF == ord('q'):
+        break
+vue.release()
+cv2.destroyAllWindows()
---
+++ Day5/back_estimation.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('contour.jpg',0)
+
+# Background Subtraction
+fgbg = cv2.createBackgroundSubtractorMOG2()
+fgmask = fgbg.apply(img)
+# Display the original image and the foreground mask
+cv2.imshow('Original Image', img)
+cv2.imshow('Foreground Mask', fgmask)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
